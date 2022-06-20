ed123854ebc971747af26e8fe5e53c2e66695c49
jmmyauh <meiyuki02211225@gmail.com>
2022-06-20 13:06:13 +0900

    third commit

--- assignmentA.py
+++ assignmentA.py
@@ -1,3 +1,21 @@
 users = ["Bob", "Tom", "Ken"]
+
 int_number = [1, 2, 3, 4, 5]
+
 bob_info = ["Bob", "Dylan", 79]
+
+# 次のmembersというリストから "Bob" と "Tom" を取得して出力してください
+members = ["Bob", "Tom", "Ken"]
+print(members[0])
+print(members[1])
+
+# 次のリストを利用して、"Name: Bob Dylan, Age: 79"と出力してください
+bob_info = ["Bob", "Dylan", 79]
+print("Name:" + bob_info[0] + bob_info[1] + "Age" + str(bob_info[2]))
+
+# for を使って odd_numbers の各要素を出力してください
+for odd_numbers in range(1, 10, 2):
+    print(odd_numbers)
+
+# for を使って even_numbers のそれぞれの値を2倍した値を出力してください
+
