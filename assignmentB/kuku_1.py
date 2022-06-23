20f9c9fc9a06656e5de965e6d809bf22dcf33491
jmmyauh <meiyuki02211225@gmail.com>
2022-06-23 13:59:55 +0900

    九九1さっきの間違い

--- assignmentB/kuku_1.py
+++ assignmentB/kuku_1.py
@@ -1,5 +1,5 @@
-for r in range(1, 10):
-    for c in range(1, 10):
-        print(f"{r*c:4d}", end="")
-    print("\n", end="")
+for i in range(1, 10):
+    for j in range(1, 10):
+            print(i * j, end=' ')
+    print()
 
